93cb2856771cfc602d2873f3c167dc3a0e0e1364
Sempank <wwwoleksiy12345@gmail.com>
2021-07-04 15:04:03 +0300

    Updates

---
+++ precache-manifest.cf7ff4d3fe362f15fcc0222f20760b36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6279591fb5de47c59e8f6499deb04144",
+    "url": "/photo-like/index.html"
+  },
+  {
+    "revision": "b5ecda5c9d086abb33cb",
+    "url": "/photo-like/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "b23b1b1fbbcb0b733650",
+    "url": "/photo-like/static/css/main.94e51bb5.chunk.css"
+  },
+  {
+    "revision": "b5ecda5c9d086abb33cb",
+    "url": "/photo-like/static/js/2.d2201950.chunk.js"
+  },
+  {
+    "revision": "b23b1b1fbbcb0b733650",
+    "url": "/photo-like/static/js/main.7290c30d.chunk.js"
+  },
+  {
+    "revision": "5f6a4ca6aa039a7d4920",
+    "url": "/photo-like/static/js/runtime~main.cf0bf215.js"
+  },
+  {
+    "revision": "b9da2244b48e97577590dd8137200f51",
+    "url": "/photo-like/static/media/memphis-mini-dark.b9da2244.png"
+  }
+]);
